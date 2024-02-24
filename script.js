d271f5ea391e7e5db8ce7c99f2b4280f92afb577
PedroCavallaro <pedrolemecavallaro@gmail.com>
2024-02-24 15:53:04 -0300

    refactor: switched typeod value to !value on hasValue function

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const ecoScoreMap = {
 };
 
 const hasValue = (value) => {
-    if (typeof value === "undefined" || value.length === 0) {
+    if (!value || value.length === 0) {
         return "Dado não encontrado";
     }
     return value;
