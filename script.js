7d2e6cd66e16961a78a05d6cc4ace11c6d0dbfb4
PedroCavallaro <pedrolemecavallaro@gmail.com>
2024-02-24 01:41:57 -0300

    feat: added nova score map, nutri score map and ecoscore map to handle food classification

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const productName = document.querySelector(
     "#field_generic_name > .field_value > [itemprop='description']"
 );
-
 const attributes = document.querySelectorAll(".attr_text > span");
 const ingredients = document.querySelector(".panel_text");
 const usedName = document.querySelector(".title-1");
@@ -12,8 +11,28 @@ const brands = Array.from(
 );
 const [nutriScore, nova1, ecoScore] = attributes;
 
+const nutriScoreMap = {
+    "Qualidade nutricional muito boa": "A",
+    "Qualidade nutricional boa": "B",
+    "Qualidade nutricional média": "C",
+    "Qualidade nutricional baixa": "D",
+    "Impacto ambiental alto": "E",
+    "Faltam dados para calcular o Nutri-Score": "Dado não encontrado",
+};
+
 const novaScoreMap = {
-    "": "A",
+    "Alimentos não processados ​​ou minimamente processados": "1",
+    "Ingredientes culinários processados": "2",
+    "Alimentos processados": "3",
+    "Alimentos ultra-processados": "4",
+    "Nível desconhecido de processamento do alimento": "Dado não encontrado",
+};
+const ecoScoreMap = {
+    "Impacto ambiental muito baixo": "A",
+    "Baixo impacto ambiental": "B",
+    "Impacto ambiental moderado": "C",
+    "Má qualidade nutricional": "D",
+    "Impacto ambiental desconhecido": "Dado não encontrado",
 };
 
 const hasValue = (value) => {
@@ -30,14 +49,21 @@ const formatIngredients = (ingredientsString) => {
     return "Dado não encontrado";
 };
 
+const mapAttribute = (text, map) => {
+    return {
+        classification: hasValue(map[text]),
+        cause: hasValue(text),
+    };
+};
+
 const productObj = {
     usedName: hasValue(usedName?.innerHTML),
     name: hasValue(productName?.innerHTML),
     brands: hasValue(brands),
     attributes: {
-        nova1: hasValue(nutriScore?.innerHTML),
-        nutriScore: hasValue(nova1?.innerHTML),
-        ecoScore: hasValue(ecoScore?.innerHTML),
+        nova1: mapAttribute(nova1?.innerHTML, novaScoreMap),
+        nutriScore: mapAttribute(nutriScore?.innerHTML, nutriScoreMap),
+        ecoScore: mapAttribute(ecoScore?.innerHTML, ecoScoreMap),
     },
     quantity: hasValue(quantity?.innerHTML),
     ingredients: formatIngredients(ingredients.innerHTML),
