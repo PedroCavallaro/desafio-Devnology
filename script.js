b45566fdd8962555020bb24414d416cc2619d90b
PedroCavallaro <pedrolemecavallaro@gmail.com>
2024-02-24 00:59:41 -0300

    feat: added usedName on product object

--- script.js
+++ script.js
@@ -2,27 +2,45 @@ const productName = document.querySelector(
     "#field_generic_name > .field_value > [itemprop='description']"
 );
 
-const brands = document.querySelectorAll("[itemprop='brand']");
 const attributes = document.querySelectorAll(".attr_text > span");
-const ingredients = document.querySelector(".panel_text").value;
+const ingredients = document.querySelector(".panel_text");
+const usedName = document.querySelector(".title-1");
+const quantity = document.querySelector("#field_quantity > .field_value");
+const brands = Array.from(
+    document.querySelectorAll("[itemprop='brand']"),
+    (brand) => brand.innerHTML
+);
+const [nutriScore, nova1, ecoScore] = attributes;
+
+const novaScoreMap = {
+    "": "A",
+};
 
 const hasValue = (value) => {
-    if (!value) {
+    if (typeof value === "undefined" || value.length === 0) {
         return "Dado não encontrado";
     }
     return value;
 };
 
-const productObj = {
-    name: "",
-    brands: [],
-    attributes: {
-        nova1: "",
-        nutriScore: "",
-        ecoScore: "",
-    },
-    quantity: "",
-    ingredients: [],
+const formatIngredients = (ingredientsString) => {
+    if (hasValue(ingredientsString)) {
+        return String(ingredientsString).trim().split("\n");
+    }
+    return "Dado não encontrado";
 };
 
-const [nutriScore, nova1, ecoScore] = attributes;
+const productObj = {
+    usedName: hasValue(usedName?.innerHTML),
+    name: hasValue(productName?.innerHTML),
+    brands: hasValue(brands),
+    attributes: {
+        nova1: hasValue(nutriScore?.innerHTML),
+        nutriScore: hasValue(nova1?.innerHTML),
+        ecoScore: hasValue(ecoScore?.innerHTML),
+    },
+    quantity: hasValue(quantity?.innerHTML),
+    ingredients: formatIngredients(ingredients.innerHTML),
+};
+
+console.log(productObj);
